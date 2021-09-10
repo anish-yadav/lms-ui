2d5152d9d2250f34f3f0fdd01773f9373d5470ab
Anish Kumar Yadav <hney1000.ay1@gmail.com>
2021-09-10 11:43:17 +0530

    removed transition from sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider, Text } from "@chakra-ui/react";
 import Login from "./components/Authentication/Login";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { UserContext, UserProvider } from "./context/user";
 
 export const App = () => (
   <ChakraProvider>
-    <Sidebar>
-      <Login></Login>
-    </Sidebar>
+    <UserProvider>
+      <Sidebar>
+        <Login />
+      </Sidebar>
+    </UserProvider>
   </ChakraProvider>
 );
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -82,7 +82,7 @@ interface SidebarProps extends BoxProps {
 const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
   return (
     <Box
-      transition="3s ease"
+      // transition="3s ease"
       bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
       borderRightColor={useColorModeValue("gray.200", "gray.700")}
