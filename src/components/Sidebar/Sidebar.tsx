b1ff2bd885c99df9eb1215515d84cbd9470567d2
Anish Kumar Yadav <hney1000.ay1@gmail.com>
2021-09-30 16:34:42 +0530

    chore: removed topbar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,52 +1,42 @@
 import {
-  Avatar,
   Box,
   BoxProps,
   CloseButton,
+  Divider,
   Drawer,
   DrawerContent,
   Flex,
   FlexProps,
-  HStack,
   Icon,
-  IconButton,
   Link,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
   Text,
-  useColorMode,
   useColorModeValue,
   useDisclosure,
-  VStack,
 } from "@chakra-ui/react";
-import { ReactNode, ReactText, useContext } from "react";
+import { ReactNode, ReactText } from "react";
 import { IconType } from "react-icons";
-import { FaMoon, FaSun } from "react-icons/fa";
 import {
-  FiChevronDown,
   FiCompass,
   FiHome,
-  FiMenu,
+  FiLogOut,
   FiSettings,
   FiStar,
   FiTrendingUp,
+  FiUser,
 } from "react-icons/fi";
-import { useLocation } from "react-router";
-import { UserContext } from "../../context/user";
+import { Link as ReactRouterLink, useLocation } from "react-router-dom";
 
 interface LinkItemProps {
   name: string;
   icon: IconType;
+  to: string;
 }
 const LinkItems: Array<LinkItemProps> = [
-  { name: "Home", icon: FiHome },
-  { name: "Trending", icon: FiTrendingUp },
-  { name: "Explore", icon: FiCompass },
-  { name: "Favourites", icon: FiStar },
-  { name: "Settings", icon: FiSettings },
+  { name: "Home", icon: FiHome, to: "/" },
+  { name: "Trending", icon: FiTrendingUp, to: "/course" },
+  { name: "Explore", icon: FiCompass, to: "/message" },
+  { name: "Favourites", icon: FiStar, to: "/chats" },
+  { name: "Settings", icon: FiSettings, to: "/404" },
 ];
 
 export default function Sidebar({ children }: { children: ReactNode }) {
@@ -76,8 +66,9 @@ export default function Sidebar({ children }: { children: ReactNode }) {
         </DrawerContent>
       </Drawer>
       {/* mobilenav */}
-      <MobileNav onOpen={onOpen} hidden={hidden} />
-      <Box ml={{ base: 0, md: hidden ? 0 : 60 }}>{children}</Box>
+      <Box ml={{ base: 0, md: hidden ? 0 : 48 }} p="2">
+        {children}
+      </Box>
     </Box>
   );
 }
@@ -93,139 +84,60 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
       bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
       borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
+      w={{ base: "full", md: "48" }}
       pos="fixed"
       h="full"
+      justifyContent="space-between"
       {...rest}>
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+      <Flex direction="column" justifyContent="space-between" h="inherit">
+        <Flex direction="column">
+          <Flex h="20" px="4" alignItems="center" justifyContent="space-between">
+            <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+              Logo
+            </Text>
+            <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+          </Flex>
+          {LinkItems.map((link) => (
+            <NavItem to={link.to} key={link.name} icon={link.icon}>
+              {link.name}
+            </NavItem>
+          ))}
+          <Divider />
+        </Flex>
+
+        <Flex direction="column">
+          <Link as={ReactRouterLink} to={"/profile"}>
+            <Flex align="center" py="2" pl="4">
+              <Icon mr="6" fontSize="16" as={FiUser} />
+              Account
+            </Flex>
+          </Link>
+          <Link as={ReactRouterLink} to={"/profile"}>
+            <Flex align="center" py="2" pl="4" color="red.400">
+              <Icon mr="6" fontSize="16" as={FiLogOut} />
+              Logout
+            </Flex>
+          </Link>
+        </Flex>
       </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
     </Box>
   );
 };
 
 interface NavItemProps extends FlexProps {
   icon: IconType;
+  to: string;
   children: ReactText;
 }
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+const NavItem = ({ icon, children, to, ...rest }: NavItemProps) => {
+  const loc = useLocation();
+  const path = loc.pathname;
   return (
-    <Link href="#" style={{ textDecoration: "none" }}>
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: "cyan.400",
-          color: "white",
-        }}
-        {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "white",
-            }}
-            as={icon}
-          />
-        )}
+    <Link variant={path === to ? "navActive" : "nav"} my="2" as={ReactRouterLink} to={to}>
+      <Flex align="center" py="2" pl="4">
+        {icon && <Icon mr="6" fontSize="16" as={icon} />}
         {children}
       </Flex>
     </Link>
-  );
-};
-
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
-}
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  const { toggleColorMode } = useColorMode();
-  const { user } = useContext(UserContext);
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}>
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text
-        display={{ base: "flex", md: "none" }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold">
-        Logo
-      </Text>
-
-      <HStack spacing={{ base: "0", md: "6" }}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          onClick={() => toggleColorMode()}
-          aria-label="open menu"
-          icon={<SwitchIcon />}
-        />
-        <Flex alignItems={"center"}>
-          <Menu>
-            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: "none" }}>
-              <HStack>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2">
-                  <Text fontSize="sm">{user && user.name}</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    {user && user.type}
-                  </Text>
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("white", "gray.900")}
-              borderColor={useColorModeValue("gray.200", "gray.700")}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
-      </HStack>
-    </Flex>
   );
 };
